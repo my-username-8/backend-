130d2759e3c46b5fefe19ae0012365946c29740f
Aastha <aasthaanand193@hotmail.com>
2025-06-11 12:48:37 +0530

    setup

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ const app = express();
 const PORT = 3000;
 
 //CORS setup
-const allowedOrigins = [
-  process.env.FRONTEND_URL, // Example: 'https://your-netlify-app-domain.netlify.app'
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow the request
-    } else {
-      callback(new Error("Not allowed by CORS")); // Deny the request
-    }
-  },
-};
+// const allowedOrigins = [
+//   process.env.FRONTEND_URL, // Example: 'https://your-netlify-app-domain.netlify.app'
+// ];
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true); // Allow the request
+//     } else {
+//       callback(new Error("Not allowed by CORS")); // Deny the request
+//     }
+//   },
+// };
 
 //MIDDLEWARE
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 //middleware to parse incoming request bodies with json payloads
 app.use(express.json());
 
